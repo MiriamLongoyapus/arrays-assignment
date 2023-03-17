e56bc46b460897ee80a000eba51ade1c7573fc66
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-03-17 19:36:42 +0300

    arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,27 @@
+//arr1=[3,7,34,90,12]
+let array=[3,7,34,90,12];
+let lastElement= array.slice(-1);
+console.log(lastElement);
+
+
+//arr2=[true,"green","where",12,56]
+arr2=[true,"green","where",12,56]
+let lastelement=arr2.slice(-1);
+console.log(lastelement);
+
+//myPets=["cow","python","snake","dog"]
+const myPets=["cow","python","snake","dog"];
+console.log(myPets.join(''));
+
+//var arr3=[-5,9,5,3,2,-3,6,8,4,1]
+const arr3=[-5,9,5,3,2,-3,6,8,4,1];
+arr3.sort();
+console.log(arr3);
+
+//var arr=["apple","mango","apple","orange","mango","mango"]
+//var arr=["apple","mango","apple","orange","mango","mango"];
+//let uniqueArr=[new set (arr)];
+//console.log(uniqueArr);
+let word=["lufituable"];
+word.sort();
+console.log(word.sort());
